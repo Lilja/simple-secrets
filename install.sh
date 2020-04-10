593837a992f51ee0a4a2e41e1fae826280b0f8c4
Erik Lilja <erikvlilja@gmail.com>
2020-04-10 14:00:42 +0000

    Unfuck the installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl https://raw.githubusercontent.com/Lilja/simple-secrets/master/simple_secret.py
+curl https://raw.githubusercontent.com/Lilja/simple-secrets/master/simple_secret.py >> simple_secret.py
 
 chmod +x simple_secret.py
